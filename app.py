95d0ae11143813a20f1bd7a7e0c9221ebcdf9be9
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 03:43:38 -0300

    Add Streamlit configuration and theme support

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+
+# Importou? Agora sim podemos usar st.
 st.markdown('<link rel="stylesheet" href="assets/css/theme.css">', unsafe_allow_html=True)
+
+st.set_page_config(
+    page_title="SIG Job Architecture",
+    layout="wide"
+)
