8a0113337d19b24aa10c7b8469544bfa9eb140de
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 01:28:27 -0300

    Add files via upload

---
+++ pages/2_Job_Families.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+from pathlib import Path
+
+st.set_page_config(page_title="Job Families", page_icon="📌", layout="wide")
+
+css = Path("assets/css/theme.css").read_text()
+st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+
+
+with st.sidebar:
+    st.logo("assets/icons/SIG_Logo_RGB_Black.svg", size="medium")
+
+
+st.markdown(f"""
+<h1 class='page-title'>
+<img src='./assets/icons/people_employees.png' width='64' style='vertical-align:middle;margin-right:10px;'>
+Job Families
+</h1>
+""", unsafe_allow_html=True)
---
+++ pages/7_Dashboard.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+from pathlib import Path
+
+st.set_page_config(page_title="Dashboard", page_icon="📌", layout="wide")
+
+css = Path("assets/css/theme.css").read_text()
+st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+
+
+with st.sidebar:
+    st.logo("assets/icons/SIG_Logo_RGB_Black.svg", size="medium")
+
+
+st.markdown(f"""
+<h1 class='page-title'>
+<img src='./assets/icons/data_2_performance.png' width='64' style='vertical-align:middle;margin-right:10px;'>
+Dashboard
+</h1>
+""", unsafe_allow_html=True)
