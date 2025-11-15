6f22a5f00da08131221a342e02a7f910fc2753c4
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 20:45:01 -0300

    Add files via upload

--- pages/1_Job_Architecture.py
+++ pages/1_Job_Architecture.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-# LOGO em todas as páginas
+# LOGO sempre na sidebar
 with st.sidebar:
     st.image("assets/icons/SIG_Logo_RGB_Black.png", width=140)
-    st.markdown("<br>", unsafe_allow_html=True)
+    st.write("")
 
-# Cabeçalho com ícone
+# TÍTULO COM PNG NATIVO
 st.markdown(f"""
 <div style='display:flex; align-items:center; gap:16px;'>
-    <img src="/app/assets/icons/governance.png" width="48">
+    <img src="assets/icons/governance.png" width="48">
     <h1>Job Architecture</h1>
 </div>
 """, unsafe_allow_html=True)
