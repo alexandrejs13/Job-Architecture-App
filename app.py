bdd6dc6337c0e7ae3a64145b010335637a301b79
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 02:03:16 -0300

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title='Job Architecture App',page_icon='🏠',layout='wide')
-st.markdown('<h1 class="page-title">🏠 Home</h1>',unsafe_allow_html=True)
-st.write('Bem-vindo. Use o menu.')
