96db15febad9db52bec9a1f7194d1d270fee4031
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 20:52:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title="Job Architecture App", layout="wide")
 
-# LOGO sempre acima do menu
+# LOGO APENAS no arquivo principal para corrigir a posição e o "pisca"
 with st.sidebar:
     st.image("assets/icons/SIG_Logo_RGB_Black.png", width=140)
-    st.write("")
+    st.markdown("---") # Linha para separar o logo do menu de páginas
 
 st.title("Bem-vindo ao Job Architecture App")
 st.write("Selecione uma página no menu à esquerda.")
