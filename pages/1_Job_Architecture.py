51db5b40eba546f1e61d2d17c1834873007541f6
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 20:57:05 -0300

    Add files via upload

--- pages/1_Job_Architecture.py
+++ pages/1_Job_Architecture.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
-# O LOGO foi movido para o app.py
-# Cabeçalho com ícone usando recursos nativos (st.columns e st.image)
+# Cabeçalho com ícone NATIVO
 col1, col2 = st.columns([0.1, 0.9])
-
 with col1:
     st.image("assets/icons/governance.png", width=48)
-
 with col2:
     st.header("Job Architecture")
 
+st.markdown("---")
 st.write("Conteúdo da página Job Architecture.")
--- pages/7_Dashboard.py
+++ pages/7_Dashboard.py
@@ -1,16 +1,11 @@
 import streamlit as st
 
-# LOGO sempre na sidebar
-with st.sidebar:
-    st.image("assets/icons/SIG_Logo_RGB_Black.png", width=140)
-    st.write("")
+# Cabeçalho com ícone NATIVO
+col1, col2 = st.columns([0.1, 0.9])
+with col1:
+    st.image("assets/icons/data_2_perfromance.png", width=48)
+with col2:
+    st.header("Dashboard")
 
-# TÍTULO COM PNG NATIVO
-st.markdown(f"""
-<div style='display:flex; align-items:center; gap:16px;'>
-    <img src="assets/icons/data_2_perfromance.png" width="48">
-    <h1>Dashboard</h1>
-</div>
-""", unsafe_allow_html=True)
-
+st.markdown("---")
 st.write("Conteúdo da página Dashboard.")
