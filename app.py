a68bc04bde86311a6b3a886503ad1163f4e9ada6
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 23:59:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Job Architecture", layout="wide", icon="house")
+st.set_page_config(page_title="Job Architecture", layout="wide")
 
 st.title("Job Architecture Home")
 st.write("Welcome to the SIG Job Architecture App.")
